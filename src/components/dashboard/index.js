f5b705d6d8e38b6d6db75b3d2c0edf2740cd2207
Musab Momin <musab.momin@kapso.in>
2022-09-12 17:12:15 +0530

    linked google and facebook to single account now user can connect or login from both facebook and google

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,7 +1,9 @@
 import React from 'react'
-import { Button, Divider, Drawer } from 'rsuite'
+import { Alert, Button, Divider, Drawer } from 'rsuite'
 import { useProfile } from '../../context/Profile.context'
+import { database } from '../../misc/firebase';
 import EditableInput from '../EditableInput';
+import ProviderBlock from './ProviderBlock';
 
 
 // we named this component file as index.js bcoz while importing this file we just have to mention the folder name  import ./dashboar;
@@ -11,7 +13,15 @@ const Dashboard = ({ onSignout }) => {
   const { profile } = useProfile();
 
   const onSave = async (newData) =>{
-    console.log(newData);
+    const usernameRef = database.ref(`/profiles/${profile.uid}`).child('name');
+    
+    try{
+      await usernameRef.set(newData)
+      Alert.success('Username has been updated', 3000);
+    }catch(err){
+      Alert.error(err.message, 3000);
+    }
+  
   }
 
   return (
@@ -21,12 +31,13 @@ const Dashboard = ({ onSignout }) => {
       </Drawer.Header>
       <Drawer.Body>
         <h3>Hey, { profile.name} </h3>
+        <ProviderBlock />
         <Divider />
         <EditableInput 
         name = "nickname"
         initialValue = { profile.name }
         onSave = { onSave }
-        label = { <h6 className='mb-2'>Nickname</h6> }
+        label = { <h6 className='mb-2'>Username</h6> }
         />
       </Drawer.Body>
       <Drawer.Footer>

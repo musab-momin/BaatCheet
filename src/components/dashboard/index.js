2dd7bcb4562f89ad7edf85af6624bab921fc66c8
Musab Momin <musab.momin@kapso.in>
2022-09-12 11:11:41 +0530

    created reusable and editable input componenet

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import { Button, Drawer } from 'rsuite'
+import { Button, Divider, Drawer } from 'rsuite'
 import { useProfile } from '../../context/Profile.context'
+import EditableInput from '../EditableInput';
 
 
 // we named this component file as index.js bcoz while importing this file we just have to mention the folder name  import ./dashboar;
@@ -9,6 +10,10 @@ const Dashboard = ({ onSignout }) => {
 
   const { profile } = useProfile();
 
+  const onSave = async (newData) =>{
+    console.log(newData);
+  }
+
   return (
     <>
       <Drawer.Header>
@@ -16,6 +21,13 @@ const Dashboard = ({ onSignout }) => {
       </Drawer.Header>
       <Drawer.Body>
         <h3>Hey, { profile.name} </h3>
+        <Divider />
+        <EditableInput 
+        name = "nickname"
+        initialValue = { profile.name }
+        onSave = { onSave }
+        label = { <h6 className='mb-2'>Nickname</h6> }
+        />
       </Drawer.Body>
       <Drawer.Footer>
         <Button block color='red' onClick={onSignout}>

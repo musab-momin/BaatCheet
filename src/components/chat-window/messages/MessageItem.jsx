5105b62ecccc2a8889db681281c89f4a26cea062
Musab Momin <musab.momin@kapso.in>
2022-09-14 19:35:16 +0530

    message like is done

--- src/components/chat-window/messages/MessageItem.jsx
+++ src/components/chat-window/messages/MessageItem.jsx
@@ -2,16 +2,18 @@ import React, { memo } from 'react';
 import { Button } from 'rsuite';
 import TimeAgo from 'timeago-react';
 import { useActiveRoom } from '../../../context/active.room.context';
-import { useHover } from '../../../misc/custom-hooks';
+import { useHover, useMediaQuery } from '../../../misc/custom-hooks';
 import { auth } from '../../../misc/firebase';
 import ProfileAvatar from '../../dashboard/ProfileAvatar';
+import IconContolBtn from './IconContolBtn';
 import PresenceIndicator from './PresenceIndicator';
 import ProfileModal from './ProfileModal';
 
-const MessageItem = ({ message, handleAdmin }) => {
-  const { author, createdAt, text } = message;
+const MessageItem = ({ message, handleAdmin, handleLike }) => {
+  const { author, createdAt, text, likes, likeCount } = message;
 
   const [selfRef, isHovered] = useHover();
+  const isMobile = useMediaQuery('(max-width: 990px)');
 
   const isAdmin = useActiveRoom(val => val.isAdmin);
   const admins = useActiveRoom(val => val.admins);
@@ -20,6 +22,9 @@ const MessageItem = ({ message, handleAdmin }) => {
   const isAuthor = auth.currentUser.uid === author.uid;
   const canGrantAdminPermission = isAdmin && !isAuthor;
 
+  // checking the loggedin user have signed the message or not
+  const isLiked = likes && Object.keys(likes).includes(auth.currentUser.uid);
+  const canShowIcon = isMobile || isHovered   // show like icon only eighter we are hovering on it or we are in mobile device
 
   return(
     <li className={`padded mb-1 cursor-pointer ${isHovered ? 'bg-black-02' : ''}`} ref={selfRef}>
@@ -37,6 +42,14 @@ const MessageItem = ({ message, handleAdmin }) => {
             <TimeAgo 
             datetime={createdAt}
             className="font-normal text-black-45 ml-2"
+            />
+            <IconContolBtn 
+            { ...(isLiked ? {color: 'red'} : {}) }
+            isVisible = {canShowIcon}
+            iconName = 'Heart'
+            tooltip = {!isLiked ? 'give it a like' : 'revoke heart' }
+            onClick = {() => handleLike(message.id)}
+            badgeContent = {likeCount}
             />
         </div>
         <div>
--- src/components/chat-window/messages/index.jsx
+++ src/components/chat-window/messages/index.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import { useParams } from 'react-router';
 import { Alert } from 'rsuite';
-import { database } from '../../../misc/firebase';
+import { auth, database } from '../../../misc/firebase';
 import { transformToArrayById } from '../../../misc/helper';
 import MessageItem from './MessageItem';
 
@@ -45,10 +45,32 @@ const Messages = () => {
     Alert.info(alertMssg, 3000);
   }, [chatId])
 
+
+  const handleLike = useCallback(async (mssgId) =>{
+    
+    const { uid } = auth.currentUser;
+    const messageRef = database.ref(`/messages/${mssgId}`);
+    await messageRef.transaction(mssg => {
+      if(mssg){
+       if(mssg.likes && mssg.likes[uid]){
+        mssg.likeCount -= 1
+        mssg.likes[uid] = null
+        } else {
+          mssg.likeCount += 1
+          if(!mssg.likes){
+            mssg.likes = {}
+          }
+          mssg.likes[uid] = true
+        }
+      }
+      return mssg;
+    })
+  }, [])
+
   return (
     <ul className='msg-list custom-scroll'>
       {isChatEmpty && <li> No messages yet! </li>}
-      { showMessages && messages.map( mssg => <MessageItem key={mssg.id} message = {mssg} handleAdmin={handleAdmin} /> ) }
+      { showMessages && messages.map( mssg => <MessageItem key={mssg.id} message = {mssg} handleAdmin={handleAdmin} handleLike={handleLike} /> ) }
     </ul>
   )
 }

9f2fa893a3e0c3dbd92b4b29926b0c6ec41d4dee
Musab Momin <musab.momin@kapso.in>
2022-09-14 12:13:55 +0530

    chat message displayed is done

--- src/components/chat-window/messages/MessageItem.jsx
+++ src/components/chat-window/messages/MessageItem.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import TimeAgo from 'timeago-react';
 import ProfileAvatar from '../../dashboard/ProfileAvatar';
+import ProfileModal from './ProfileModal';
 
 const MessageItem = ({ message }) => {
   const { author, createdAt, text } = message;
@@ -11,7 +12,7 @@ const MessageItem = ({ message }) => {
     <li className='padded mb-1'>
         <div className='d-flex align-items-center font-bolder mb-1'>
             <ProfileAvatar src={ author.avatar } name={author.name} className='ml-1' size='xs' />
-            <span className='ml-2'>{author.name}</span>
+            <ProfileModal author={author} />
             <TimeAgo 
             datetime={createdAt}
             className="font-normal text-black-45 ml-2"
---
+++ src/components/chat-window/messages/ProfileModal.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Button, Modal } from 'rsuite'
+import { useDrawer } from '../../../misc/custom-hooks';
+import ProfileAvatar from '../../dashboard/ProfileAvatar';
+
+const ProfileModal = ({ author }) => {
+    
+    const { isOpen, open, close } = useDrawer();
+    const firstname = author.name.split(' ')[0]
+    const membersince = new Date(author.createdAt).toLocaleDateString();
+
+    return (
+    <>
+        <Button onClick={open} appearance='link' className='p-0 ml-1 text-black'>
+            { firstname }
+        </Button>
+        <Modal show={ isOpen } onHide={ close }>
+            <Modal.Header>
+                <Modal.Title> {firstname} profile </Modal.Title>
+            </Modal.Header>
+            <Modal.Body className='text-center'>
+                <ProfileAvatar 
+                src={ author.avatar } 
+                name={author.name} 
+                className='width-150 height-150 img-fullsize font-huge' 
+                />
+                <h4 className='mt-2'>{ author.name }</h4>
+                <p>Member since { membersince }</p>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button block onClick={close}>
+                    Close
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    </>
+  )
+}
+
+export default ProfileModal
